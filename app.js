2a7ef0c9cc5b5da95e9ed6094eeeb97aa86f629d
Mehedi Hassan <mehedy.mmc272@gmail.com>
2022-11-12 23:10:10 +0600

    First Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const https = require('https');
+const { dirname } = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (request, response) => {
+  const query = request.body.cityName;
+  const unit = 'metric';
+  const apiKey = '8e694bcaea608767af846c7d3d65fb11';
+  const url = ` https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`;
+  https.get(url, (res) => {
+    res.on('data', (data) => {
+      const weatherData = JSON.parse(data);
+      const weatherDescription = weatherData.weather[0].description;
+
+      const temp = weatherData.main.temp;
+      const icon = weatherData.weather[0].icon;
+      const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      response.write(
+        `<p>The Condition of Weather is ${weatherDescription}.</p>`
+      );
+      response.write(
+        `<h2>The temparature of ${weatherData.name} is now ${temp}</h2>`
+      );
+
+      response.write(`<img src="${imgUrl}" alt="">`);
+
+      response.send();
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
